19b30c66c70feb75d59f3ffd9e155d567bb3d622
Subh <subhaushsingh@gmail.com>
2025-08-27 14:14:27 +0530

    Optional property access property

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let age = 5;
 let course = 'Typescript';
 let numbers = [];
@@ -8,3 +9,22 @@ user.push(2, 'B');
 console.log(user);
 let mySize = "m";
 console.log(mySize);
+function calculateTax(income, taxYear = 2025) {
+    if (income < 0)
+        return 0;
+    return taxYear;
+}
+let employee = { id: 1, name: 'mosh' };
+let std1 = {
+    id: 1,
+    name: 'mosh',
+    randomFunction: (date) => {
+        console.log(date);
+    }
+};
+let quantity = 100;
+function getCustomer(id) {
+    return id == 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,52 @@ const enum Size{Small='s',Medium='m',Large='l'}
 let mySize : Size = Size.Medium;
 console.log(mySize);
 
-
+//function
 function calculateTax(income: number,taxYear=2025): number{
     if(income<0) return 0;
     return taxYear;
 }
+
+
+//Object
+let employee:{
+    readonly id:number,
+    name:string
+} = {id:1,name:'mosh'}
+
+//employee.id = 2 shows error as the property is readonly
+
+type Student={
+    readonly id:number,
+    name:string,
+    randomFunction: (date:Date) => void
+}
+
+let std1 : Student = {
+    id:1,
+    name: 'mosh',
+    randomFunction: (date:Date)=>{
+        console.log(date);
+    }
+
+}
+
+
+//Literal annotation
+type Quantity = 50 | 100;
+let quantity:Quantity = 100;
+
+
+//Optional chaining
+
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id:number) : Customer | null | undefined{
+    return id == 0 ? null : {birthday: new Date()};
+}
+
+let customer = getCustomer(0);
+//Optional property access operator
+console.log(customer?.birthday?.getFullYear());
